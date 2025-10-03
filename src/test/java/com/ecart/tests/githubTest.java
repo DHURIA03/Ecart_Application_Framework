2f405dd37230e86a858a59b0778e8e41a08ac282
unknown <ashishdhuri08@gmail.com>
2025-10-03 22:58:49 +0530

    Added new test class for git checking

---
+++ src/test/java/com/ecart/tests/githubTest.java
@@ -0,0 +1,16 @@
+package com.ecart.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class githubTest 
+{
+	@Test(priority=1)
+	public void validateGitTestA() 
+	{
+		
+		System.out.println("This is new testcase");
+		
+	}
+
+}
