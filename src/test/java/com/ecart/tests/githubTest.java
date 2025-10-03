077fdef8bfaeb47536cf66533dc9bf48db6413cf
unknown <ashishdhuri08@gmail.com>
2025-10-04 00:35:00 +0530

    remove conflict

--- src/test/java/com/ecart/tests/githubTest.java
+++ src/test/java/com/ecart/tests/githubTest.java
@@ -20,7 +20,7 @@ public class githubTest
 	@Test(priority=3)
 	public void validateGitTestC() 
 	{
-		System.out.println("This is new testcase testcase C modified at branch Ashish");
+		System.out.println("Accepting merge conflict");
 	}
 
 }
